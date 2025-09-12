dc665d0899e1d89afc121efa19a042b274bd0cdd
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-09-13 02:16:42 +0900

    feat: 앱 실행시 mixed immersiveSpace에서 시작하도록 변경

--- BounceBall/App/BounceBallApp.swift
+++ BounceBall/App/BounceBallApp.swift
@@ -10,24 +10,28 @@ import SwiftUI
 @main
 struct BounceBallApp: App {
 
-    @State private var appModel = AppModel()
+  @State private var appModel = AppModel()
+  @Environment(\.openWindow) private var openWindow
+  @Environment(\.dismissWindow) private var dismissWindow
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(appModel)
-        }
-
-        ImmersiveSpace(id: appModel.immersiveSpaceID) {
-            ImmersiveView()
-                .environment(appModel)
-                .onAppear {
-                    appModel.immersiveSpaceState = .open
-                }
-                .onDisappear {
-                    appModel.immersiveSpaceState = .closed
-                }
-        }
-        .immersionStyle(selection: .constant(.full), in: .full)
+  var body: some Scene {
+    WindowGroup(id: "MainWindow") {
+      ContentView()
+        .environment(appModel)
     }
+
+    ImmersiveSpace(id: appModel.immersiveSpaceID) {
+      ImmersiveView()
+        .environment(appModel)
+        .onAppear {
+          appModel.immersiveSpaceState = .open
+          openWindow(id: "MainWindow")
+        }
+        .onDisappear {
+          appModel.immersiveSpaceState = .closed
+          dismissWindow(id: "MainWindow")
+        }
+    }
+    .immersionStyle(selection: .constant(.mixed), in: .mixed)
+  }
 }
