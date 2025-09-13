1f5bf15cd66678565b62c952339f02967c5b4af5
bongjooncha <cbj4133@naver.com>
2025-09-14 00:16:31 +0900

    feat: 공 생성

--- BounceBall/Sources/Model/BallModel.swift
+++ BounceBall/Sources/Model/BallModel.swift
@@ -4,11 +4,6 @@ import simd
 @MainActor
 @Observable
 class BallModel {
-    var isPressent: Bool = false
-    var position: SIMD3<Float> = [0, 0, 0]
-
-    func reset(){
-        position = .init(0, 0, 1)
-        isPressent = false
-    }
+    var isPresent: Bool = false
+    var position: SIMD3<Float>?
 }
--- BounceBall/Sources/View/Window/MainWindow.swift
+++ BounceBall/Sources/View/Window/MainWindow.swift
@@ -1,18 +1,31 @@
 import SwiftUI
+import simd
 
 struct MainWindow: View {
     @Environment(BallModel.self) private var ballModel
 
     var body: some View {
-        Text("주변 벽에 공을 던져 마음껏 가지고 놀아보세요")
-        .padding()
-        HStack {
-            ToggleImmersiveSpaceButton()
-            Button(ballModel.isPressent ? "공 초기화" : "공 생성하기") {
-                if ballModel.isPressent {
-                    ballModel.reset()
-                } else {
-                    ballModel.isPressent = true
+        GeometryReader3D { proxy in
+            VStack {
+                Text("주변 벽에 공을 던져 마음껏 가지고 놀아보세요")
+                .padding()
+                HStack {
+                    ToggleImmersiveSpaceButton()
+                    Button(ballModel.isPresent ? "공 초기화" : "공 생성하기") {
+                        let windowCenter = proxy.frame(in: . immersiveSpace).center
+                        let pos = SIMD3<Float>(
+                            Float(windowCenter.x),
+                            Float(windowCenter.y),
+                            Float(windowCenter.z) - 0.3
+                        )
+
+                        if ballModel.isPresent {
+                            ballModel.isPresent = false
+                        } else {
+                            // ballModel.position = pos
+                            ballModel.isPresent = true
+                        }
+                    }
                 }
             }
         }
@@ -21,5 +34,6 @@ struct MainWindow: View {
 
 #Preview {
     MainWindow()
+        .environment(AppModel())
         .environment(BallModel())
 }
