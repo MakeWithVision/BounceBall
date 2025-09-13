49bc1a3ecefa20cd2a3a82182718e7656d021336
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-09-13 18:25:21 +0900

    feat: 평면 인식하고 해당 위치에 가상 plane 생성

--- BounceBall/Sources/ImmersiveView.swift
+++ BounceBall/Sources/ImmersiveView.swift
@@ -6,25 +6,80 @@
 //
 
 import SwiftUI
+import ARKit
 import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-
-    var body: some View {
-        RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
-            }
+  @State var session = ARKitSession()
+  
+  @State var planeAnchorsSimple: [UUID: Entity] = [:]
+  
+  var body: some View {
+    RealityView { content in
+      
+    } update: { content in
+      
+      for (_, entity) in planeAnchorsSimple {
+        if !content.entities.contains(entity) {
+          content.add(entity)
         }
+      }
     }
+    .task {
+      try! await setupAndRunPlaneDetection()
+    }
+  }
+  
+  func setupAndRunPlaneDetection() async throws {
+    let planeData = PlaneDetectionProvider(alignments: [.horizontal, .vertical, ])
+    if PlaneDetectionProvider.isSupported {
+      do {
+        try await session.run([planeData])
+        for await update in planeData.anchorUpdates {
+          switch update.event {
+          case .added, .updated:
+            let anchor = update.anchor
+            
+            let planeEntitySimple = createSimplePlaneEntity(for: anchor)
+            planeAnchorsSimple[anchor.id] = planeEntitySimple
+            print("앵커ID:", anchor.id, anchor.timestamp)
+            
+          case .removed:
+            let anchor = update.anchor
+            planeAnchorsSimple.removeValue(forKey: anchor.id)
+          }
+          
+        }
+      }
+      catch {
+        print("ARKit session error \(error)")
+      }
+    }
+  }
+  
+  func createSimplePlaneEntity(for anchor: PlaneAnchor) -> Entity {
+    let extent = anchor.geometry.extent
+    let mesh = MeshResource.generatePlane(width: extent.width, height: extent.height)
+    let material = OcclusionMaterial()
+    
+    let entity = ModelEntity(mesh: mesh, materials: [material])
+    entity.transform = Transform(matrix: matrix_multiply(anchor.originFromAnchorTransform, extent.anchorFromExtentTransform))
+    
+    entity.generateCollisionShapes(recursive: true, static: true)
+    let physicsMaterial = PhysicsMaterialResource.generate(friction: 0, restitution: 1)
+    let physics = PhysicsBodyComponent(massProperties: .default, material: physicsMaterial, mode: .static)
+    entity.components.set(physics)
+    
+    return entity
+  }
 }
 
-#Preview(immersionStyle: .full) {
-    ImmersiveView()
-        .environment(AppModel())
+
+
+
+
+#Preview(immersionStyle: .mixed) {
+  ImmersiveView()
+    .environment(AppModel())
 }
